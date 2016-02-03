cfdc3fb1787268de55c38041a51aee3370778e76
AlexHatesUnicorns <schadko.ao@gmail.com>
2016-02-04 01:11:16 +0300

    sigma vector added

--- src/Grids/GridTM.cpp
+++ src/Grids/GridTM.cpp
@@ -12,10 +12,12 @@
 #include <vector>
 
 #define epsilonHost(M, N) epsilon[(M) * (sizeY) + (N)]
+#define sigmaHost(M, N) sigma[(M) * (sizeY) + (N)]
 
 GridTM::GridTM(std::size_t _sizeX, std::size_t _sizeY, float _S) :  sizeX(_sizeX), sizeY(_sizeY), Hx(sizeX*(sizeY-1), 0),
 														  Hy((sizeX-1)*sizeY, 0), Ez(sizeX*sizeY ,0),
-														  epsilon(sizeX*sizeY, 1), time(0), S(_S) {
+														  epsilon(sizeX*sizeY, 1), sigma(sizeX*sizeY, 0),
+														  time(0), S(_S) {
 	std::cout << "GridTM was initiated: \n";
 	std::cout << "\t sizeX = " << (int)(sizeX) << "px \n";
 	std::cout << "\t sizeY = " << (int)(sizeY) << "px \n";
@@ -40,5 +42,27 @@ void GridTM::readEpsilon(const std::string& fileName) {
 		}
 	}
 	epsilon.CPUtoGPU();
+	file.close();
+}
+
+void GridTM::readSigma(const std::string& fileName) {
+	std::ifstream file;
+	file.open(fileName);
+	if (!file.is_open()) {
+		std::cout << " ! Error reading file with sigma ! \n";
+		return;
+	}
+	while(!file.eof()) {
+		int xCoord(0);
+		int yCoord(0);
+		file >> xCoord;
+		file >> yCoord;
+		float sigmaValue;
+		file >> sigmaValue;
+		if (xCoord >= 0 && xCoord < sizeX && yCoord >= 0 && yCoord < sizeY) {
+			sigmaHost(xCoord, yCoord) = sigmaValue;
+		}
+	}
+	sigma.CPUtoGPU();
 	file.close();
 }

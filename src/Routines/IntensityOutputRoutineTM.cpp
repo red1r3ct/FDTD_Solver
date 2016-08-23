be5e0306703ce2fd699973351b75488d6eef543c
SchadkoAO <schadko.ao@gmail.com>
2016-08-23 17:59:15 +0300

    typo

--- src/Routines/IntensityOutputRoutineTM.cpp
+++ src/Routines/IntensityOutputRoutineTM.cpp
@@ -24,24 +24,6 @@ void IntensityOutputRoutineTM::print() {
 	}
 	file.close();
 }
-void EzOutputRoutineTM::print() {
-	std::ofstream file;
-	std::string currentFileName = fileName+"_"+std::to_string(currentTime) + ".txt";
-	file.open(currentFileName, std::ofstream::trunc);
-	if (!file.is_open()) {
-		return;
-	}
-	grid->Ez.GPUtoCPU();
-	float* Ez = grid->Ez.getHostPtr();
-	int sizeEz = grid->Ez.getSize();
-	#define Ez(M, N) Ez[(M) * (grid->sizeY) + (N)]
-	for(int i = 0; i < sizeX*sizeY; i++) {
-		int xCoord = firstX+resolutionX*(i/sizeY);
-		int yCoord = firstY+resolutionY*(i%sizeY);
-		file << xCoord << " " << yCoord << " " << Ez(xCoord, yCoord) << std::endl;
-	}
-	file.close();
-}
 
 void IntensityOutputRoutineTM::flushData() {
 	for(int i = 0; i < intensity.size(); i++) {

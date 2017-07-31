6c893b22faa355f790fae0415af45ba4a8d3a543
SchadkoAO <schadko.ao@gmail.com>
2017-08-01 00:24:05 +0300

    fix

--- world_cone_waveguide/main.py
+++ world_cone_waveguide/main.py
@@ -7,7 +7,7 @@ import argparse
 import math
 
 
-def write_eps(x_0, y_0, length, d, alpha, size_x, size_y, n_core):
+def write_eps(x_0, y_0, length, d, alpha, size_x, size_y, n_core, base_path):
     eps = []
     print('Start processing epsilon')
     for y in range(size_y):
@@ -18,10 +18,10 @@ def write_eps(x_0, y_0, length, d, alpha, size_x, size_y, n_core):
             else:
                 eps.append(1)
     print('Finish processing epsilon')
-    output_array_to_file(eps, 'epsilon.txt', size_x)
+    output_array_to_file(eps, base_path + '/epsilon.txt', size_x)
 
 
-def write_sig(x_0, y_0, length, d, d_shell, size_x, size_y, sig_shell):
+def write_sig(x_0, y_0, length, d, d_shell, size_x, size_y, sig_shell, base_path):
     sig = []
     print('Start processing sigma')
     for y in range(size_y):
@@ -32,7 +32,7 @@ def write_sig(x_0, y_0, length, d, d_shell, size_x, size_y, sig_shell):
             else:
                 sig.append(0)
     print('Finish processing sigma')
-    output_array_to_file(sig, 'sigma.txt', size_x)
+    output_array_to_file(sig, base_path + '/sigma.txt', size_x)
 
 
 def write(lam_px, n_core, sig_shell, d_over_lam, alpha, num, sim_type):
@@ -42,6 +42,13 @@ def write(lam_px, n_core, sig_shell, d_over_lam, alpha, num, sim_type):
     size_y = int(10 * lam_px + d_shell)
     x_0 = int(10 * lam_px)
     y_0 = int(size_y / 2)
+    base_path = 'cone_v1_d={d_over_lam}_a={alpha_deg}_lam={lam_px}_n={n_core}_sig={sig_shell}'.format(
+        d_over_lam=d_over_lam,
+        alpha_deg=( alpha * 180 / math.pi ),
+        lam_px=lam_px,
+        n_core=n_core,
+        sig_shell=sig_shell
+    )
     write_eps(x_0, y_0, length, d_over_lam * lam_px, alpha, size_x, size_y, n_core)
     write_sig(x_0, y_0, length, d_over_lam * lam_px, d_shell, size_x, size_y, sig_shell)
     write_conf(type, x_0 - 10 * lam_px, y_0, lam_px, size_x, size_y, num)
@@ -96,7 +103,8 @@ def write_conf(sim_type, x_source, y_source, lam_px, size_x, size_y, num):
         i_small_out_last_y=i_small_out_last_y,
     )
 
-    return conf
+    f = open(base_path + '/config.txt', 'w')
+    f.write(conf)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
